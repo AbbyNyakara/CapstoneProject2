33f1fe99590c34b69dd6fd492b5c7d2ce20f92b0
Abby Nyakara <abbynyakara@gmail.com>
2022-06-29 10:15:03 +0300

    Add popup detail

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('.recipe-close-btn');
 const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=egg';
+const modalDetail = document.querySelector('.meal-details');
 
 // Add event listeners 
 
@@ -29,28 +30,57 @@ const getFood = async () => {
               <a href="#" class="recipe-btn">Comment on Recipe</a>
             </div>
           </div>
-    `
+    `;
   })
 }
 
-window.addEventListener('load', getFood());
+window.addEventListener('load', getFood);
 
 // Add event Listener to the comments popup detail
 // Because the content is created dynamically, add an event listener to the entire list of cards
 
-const getRecipe =  (e) => {
-
+const getRecipe =  async (e) => {
   if(e.target.classList.contains('recipe-btn')) {
     let mealItem = e.target.parentElement.parentElement;
+    // console.log(mealItem);
     let foodID = mealItem.getAttribute('meal-id');
     // console.log(foodID);
     
-    fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${foodID}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-    })
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodID}`);
+    const dataFile = await res.json();
+    const mealInfo = dataFile.meals[0];
+    const html = `
+        <button class="btn recipe-close-btn" id="recipe-close-btn">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+        <div class="recipe-meal-img">
+            <img src="${mealInfo.strMealThumb}" alt="">
+        </div>
+
+        <div class="meal-details-contents">
+          <h2 class="recipe-title">${mealInfo.strMeal}</h2>
+          <div class="recipe-instruct">
+            <h3>Instructions</h3>
+            <p>${mealInfo.strInstructions}</p>
+          </div>
+        </div>
+        <h2>Comments</h2>
+    `;
+    modalDetail.innerHTML = html;
+    modalDetail.classList.remove('hide');
+    modalDetail.classList.add('show');
   }
 }
 
 mealList.addEventListener('click', getRecipe);
+// bm5Pc4YXKqXjY7TxuidW
+
+//Add event listener to the close button 
+
+modalDetail.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-xmark')) {
+    const modal = e.target.parentElement.parentElement;
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+  }
+})
